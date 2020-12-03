95a2d0341267895bf3eec1f22d414c8a68ad69e9
angtian wang <angtianwang@jhu.edu>
2020-12-03 12:19:37 -0500

    Add download scripts.

---
+++ code/download_occluder_lib.sh
@@ -0,0 +1,3 @@
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1DgBs8liddu0sf4PNcEKKkxjR-EqnOHY5' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1DgBs8liddu0sf4PNcEKKkxjR-EqnOHY5" -O OccludedLibs.zip && rm -rf /tmp/cookies.txt &&
+unzip OccludedLibs.zip && 
+rm OccludedLibs.zip
