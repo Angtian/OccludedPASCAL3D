2d74d68babff9d1b98b5fad16a4e9ee77e6cb1a0
Angtian <63261150+Angtian@users.noreply.github.com>
2020-12-25 17:50:16 -0500

    Update download_FG.sh

--- download_FG.sh
+++ download_FG.sh
@@ -2,7 +2,7 @@
 # FGL1_BGL1
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1X-xwyypLTm9vr-boLYPIPhGxcYaPHSNF' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1X-xwyypLTm9vr-boLYPIPhGxcYaPHSNF" -O OccludedPASCAL3D_FGL1_BGL1.zip &&
 # FGL2_BGL2
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1dNP8YE3RJ9Pzr_jQ11O6f6eYgYnq9RO' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1dNP8YE3RJ9Pzr_jQ11O6f6eYgYnq9RO" -O OccludedPASCAL3D_FGL2_BGL2.zip &&
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1dNP8YE3RJ9Pzr_jQ11O6f6eYgYnq9ROp' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1dNP8YE3RJ9Pzr_jQ11O6f6eYgYnq9ROp" -O OccludedPASCAL3D_FGL2_BGL2.zip &&
 # FGL3_BGL3
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1GsHCyAYnqcJsAgiih1vKpDQxzF3ouFxS' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1GsHCyAYnqcJsAgiih1vKpDQxzF3ouFxS" -O OccludedPASCAL3D_FGL3_BGL3.zip &&
 
